62b1a7b1662e0141311e02e2dffa8f2a6768bcc1
Michael Latowicki <mlat@outlook.co.il>
2023-07-31 15:34:10 +0300

    + equal_tail_intervals.log_normal

--- credints/equal_tail_intervals.py
+++ credints/equal_tail_intervals.py
@@ -1,7 +1,17 @@
 from nbag import construct
-from sympy.stats import Normal, quantile, rv
+from sympy.stats import Normal, LogNormal, quantile, rv
 from typing import Optional
+import numpy as np
 
+
+def normal_params_from_eti(credence: float, low: float, high: float) -> tuple[float, float]:
+    mean = (low+high)/2
+    assert low < high
+    assert 0 < credence < 1, 'credence must be strictly between 0 and 1'
+    
+    z = quantile(Normal("stdnorm",0,1))(0.5 + credence/2)
+    stdev = (high - low) / (2 * z)
+    return mean, stdev
 
 def normal(credence: float, bounds: list[float], name:Optional[str]=None) -> rv.RandomSymbol:
     """
@@ -9,13 +19,18 @@ def normal(credence: float, bounds: list[float], name:Optional[str]=None) -> rv.
     equal probability of being higher than bounds[1] as it does of being lower than bounds[0].
     """
     low, high = bounds
-    mean = (low+high)/2
-    assert low < high
-    assert 0 < credence < 1, 'credence must be strictly between 0 and 1'
-    
-    z = quantile(Normal("stdnorm",0,1))(0.5 + credence/2)
-    stdev = (high - low) / (2 * z)
+    mean, stdev = normal_params_from_eti(credence, low, high)
     return construct(Normal, name, mean, stdev)
+
+
+def log_normal(credence: float, bounds: list[float], name:Optional[str]=None) -> rv.RandomSymbol:
+    """
+    Returns a normal RandomSymbol that falls within <bounds> at probability <credence>, and has
+    equal probability of being higher than bounds[1] as it does of being lower than bounds[0].
+    """
+    low, high = bounds
+    mean, stdev = normal_params_from_eti(credence, np.log(low), np.log(high)) 
+    return construct(LogNormal, name, mean, stdev)
 
 
 __all__ = ["normal"]
--- test/test_credints.py
+++ test/test_credints.py
@@ -1,5 +1,6 @@
 import credints.equal_tail_intervals as ci
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_approx_equal
+import numpy as np
 
 
 def test_normal_eti():
@@ -15,6 +16,25 @@ def test_normal_eti():
     from sympy.stats import P
     effective_credence = P((x>low) & (x<high)).evalf()
     assert_almost_equal(credence, effective_credence)
+
+
+def test_log_normal_eti():
+    try:
+        ci.log_normal(0.9, [2, 4])
+    except:
+        pass
+    else:
+        assert False, "calling equal_tail_intervals.normal() outside of an assignment should fail"
+    credence = 0.9
+    low, high = 2, 4
+    x = ci.log_normal(credence, [low, high])
+    from sympy.stats import P
+    tail_weight = 0.5*(1-credence)
+    n = 100000
+    from sympy.stats import sample
+    xs = sample(x, size=n, seed=0)
+    assert_approx_equal(tail_weight, (xs < low).mean(), significant=2)
+    assert_approx_equal(tail_weight, (xs > high).mean(), significant=2)
 
 
 if __name__ == "__main__":
